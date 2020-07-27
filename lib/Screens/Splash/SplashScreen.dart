1b847f3517bff4d8e5ab93e3cb3113fccecc3c77
egpay <yasserhassan.it.95@gmail.com>
2020-07-27 17:54:21 +0200

    finish getting data in splash screen

--- lib/Screens/Splash/SplashScreen.dart
+++ lib/Screens/Splash/SplashScreen.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_ro/Screens/Splash/Components/Body.dart';
 import 'package:flutter_ro/Screens/main/main_screen.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+
+import '../../Constants.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -19,7 +22,8 @@ class StartState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    startTimer();
+    //startTimer();
+    loadAppDataFromParseServer();
   }
 
   startTimer() async {
@@ -27,8 +31,56 @@ class StartState extends State<SplashScreen> {
     return new Timer(duration, route);
   }
 
-  loadAppDataFromParseServer() async {
+  Future<void> loadAppDataFromParseServer() async {
+    await Parse().initialize(
+        keyApplicationId,
+        keyParseServerUrl,clientKey: keyClientKey);
 
+    var ApplicationINFO = await ParseObject('promo_codes').getAll();
+
+    var ProductsApiResponse = await ParseObject('products').getAll();
+
+    var AdvApiResponse = await ParseObject('Ads_Banners').getAll();
+
+    var WorkshopsApiResponse = await ParseObject('workshops').getAll();
+
+    var EventsApiResponse = await ParseObject('events').getAll();
+
+    var CategoryapiResponse = await ParseObject('category').getAll();
+
+    if (CategoryapiResponse.success){
+      for (var testObject in CategoryapiResponse.result) {
+        print('Rokna App' + ": " + testObject.toString());
+      }
+    }
+    if (ApplicationINFO.success){
+      for (var testObject in ApplicationINFO.result) {
+        print('Rokna App' + ": " + testObject.toString());
+      }
+    }
+    if (ProductsApiResponse.success){
+      for (var testObject in ProductsApiResponse.result) {
+        print('Rokna App' + ": " + testObject.toString());
+      }
+    }
+    if (AdvApiResponse.success){
+      for (var testObject in AdvApiResponse.result) {
+        print('Rokna App' + ": " + testObject.toString());
+      }
+    }
+    if (WorkshopsApiResponse.success){
+      for (var testObject in WorkshopsApiResponse.result) {
+        print('Rokna App' + ": " + testObject.toString());
+      }
+    }
+    if (EventsApiResponse.success){
+      for (var testObject in EventsApiResponse.result) {
+        print('Rokna App' + ": " + testObject.toString());
+      }
+    }
+
+    print('done');
+    route();
 
     }
 
